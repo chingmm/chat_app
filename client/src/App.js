e6348c97a2e6132cb4c11f08848551783fc96498
chingmm <chingmm19@gmail.com>
2020-12-28 18:39:31 -0600

    Added restricted routes and users showing up

--- client/src/App.js
+++ client/src/App.js
@@ -10,20 +10,25 @@ import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
 
+import {AuthProvider} from './context/auth'
+import DynamicRoute from './util/DynamicRoute'
+
 
 function App() {
 
   return (
     <ApolloProvider>
-      <BrowserRouter>
-      <Container className="pt-5">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/register" component={Register} />
-          <Route path="/login" component={Login} />
-        </Switch>
-      </Container>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Container className="pt-5">
+            <Switch>
+              <DynamicRoute exact path="/" component={Home} auithenticated/>
+              <DynamicRoute path="/register" component={Register} guest/>
+              <DynamicRoute path="/login" component={Login} guest/>
+            </Switch>
+          </Container>
+        </BrowserRouter>
+      </AuthProvider>
     </ApolloProvider>
   );
 }
---
+++ graphql/resolvers.js
@@ -0,0 +1,149 @@
+const bcrypt = require('bcryptjs')
+const { UserInputError, AuthenticationError } = require('apollo-server')
+const jwt = require('jsonwebtoken')
+const { Op } = require('sequelize')
+
+const { Message, User } = require('../models')
+const { JWT_SECRET } = require('../config/env.json')
+
+module.exports = {
+    Query: {
+      getUsers: async (_, __, { user }) => {
+
+          try {
+            if(!user) throw new AuthenticationError('Unauthenticated')
+
+              const users = await User.findAll({
+                  where: { username: { [Op.ne]: user.username }},
+              })
+
+              return users
+          } catch(err) {
+              console.log(err)
+              throw err
+          }
+      },
+      login: async (_, args) => {
+          const { username, password } = args
+          let errors = {}
+
+          try {
+            if (username.trim() === '')
+              errors.username = 'username must not be empty'
+            if (password === '') errors.password = 'password must not be empty'
+    
+            if (Object.keys(errors).length > 0) {
+              throw new UserInputError('bad input', { errors })
+            }
+    
+            const user = await User.findOne({
+              where: { username },
+            })
+    
+            if (!user) {
+              errors.username = 'user not found'
+              throw new UserInputError('user not found', { errors })
+            }
+    
+            const correctPassword = await bcrypt.compare(password, user.password)
+    
+            if (!correctPassword) {
+              errors.password = 'password is incorrect'
+              throw new UserInputError('password is incorrect', { errors })
+            }
+
+            const token = jwt.sign({ username }, JWT_SECRET, { expiresIn: 60 * 60 });
+
+              return {
+                  ...user.toJSON(),
+                  createdAt: user.createdAt.toISOString(),
+                  token
+              }
+          } catch(err) {
+            console.log(err)
+            throw err
+        }
+      }
+    },
+    Mutation: {
+        register: async (_, args) => {
+
+            let { username, email, password, confirmPassword } = args
+            let errors = {}
+
+            try {
+                // Validate input data
+                if (email.trim() === '') errors.email = 'email must not be empty'
+                if (username.trim() === '') errors.username = 'username must not be empty'
+                if (password.trim() === '') errors.password = 'password must not be empty'
+                if (confirmPassword.trim() === '') errors.confirmPassword = 'repeat password must not be empty'
+
+                if (password != confirmPassword) errors.confirmPassword = 'passwords must match'
+
+                // // Check if username / email exists
+                // const userByUsername = await User.findOne({ where: { username }})
+                // const userByEmail = await User.findOne({ where: { email }})
+
+                // if(userByUsername) errors.username = 'Username is taken'
+                // if(userByEmail) errors.email = 'Email is taken'
+
+                if (Object.keys(errors).length > 0){
+                    throw errors
+                }
+
+                // Hash password
+                password = await bcrypt.hash(password, 6)
+
+                // Create user
+                const user = await User.create({
+                    username, 
+                    email, 
+                    password,
+                })
+
+                // Return user
+                return user
+            } catch (err) {
+                console.log(err)
+                if (err.name === 'SequelizeUniqueConstraintError') {
+                    err.errors.forEach(
+                        (e) => (errors[e.path] = `${e.path} is already taken`)
+                    )
+                } else if (err.name === 'SequelizeValidationError'){
+                    err.errors.forEach(
+                        (e) => (errors[e.path] = e.message)
+                    )
+                }
+                throw new UserInputError('Bad input', { errors })
+            }
+        },
+        sendMessage: async (parent, {to, content}, { user }) => {
+            try{
+                if (!user) throw new AuthenticationError('Unauthenticated')
+
+                const recipient = await User.findOne({where: {username: to}})
+
+                if (!recipient){
+                    throw new UserInputError('User not found')
+                } else if (recipient.username === user.username){
+                    throw new UserInputError('You can\'t message yourself')
+                }
+
+                if (content.trim() === ''){
+                    throw new UserInputError('Message is empty')
+                }
+
+                const message = await Message.create({
+                    from: user.username,
+                    to,
+                    content
+                })
+
+                return message
+            }catch(err){
+                console.log(err)
+                throw err
+            }
+        }
+    },
+  }
