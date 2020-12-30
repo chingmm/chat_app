27d8771e821b2848587df273a59b61de33c25e92
chingmm <chingmm19@gmail.com>
2020-12-30 13:32:59 -0600

    Heroku diagnostic check

--- models/index.js
+++ models/index.js
@@ -9,6 +9,8 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
+  console.log(process.env.DATABASE_URL)
+  console.log(config)
   sequelize = new Sequelize(process.env.DATABASE_URL, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
