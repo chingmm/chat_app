33f04c85348c3629d3cea488d2c6e309ceefcd8c
chingmm <chingmm19@gmail.com>
2020-12-30 13:30:00 -0600

    heroku typo

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(config.use_env_variable, config);
+  sequelize = new Sequelize(process.env.DATABASE_URL, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
